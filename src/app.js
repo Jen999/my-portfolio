87f3f1cf0bd766ca7d445ef0bebef201d725dd6c
Tan Ling Jen <tanlingjen.tlj@gmail.com>
2023-07-18 16:02:47 +0800

    lightdarkmode

--- src/app.js
+++ src/app.js
@@ -1,27 +1,33 @@
-import React from "react"
-import "./style.css"
-import HomePageContent from "./components/HomePageContent"
-import HomePageLinks from "./components/HomePageLinks"
-import HomePageNavBar from "./components/HomePageNavBar"
-import AboutMeContent from "./components/AboutMeContent"
-import ToggleBox from "./components/ToggleBox"
-import ExperienceContent from "./components/ExperienceContent"
-import ProjectsContent from "./components/ProjectsContent"
-import EducationContent from "./components/EducationContent"
+import React, { createContext, useState } from "react";
+import "./style.css";
+import HomePageContent from "./components/HomePageContent";
+import HomePageLinks from "./components/HomePageLinks";
+import HomePageNavBar from "./components/HomePageNavBar";
+import AboutMeContent from "./components/AboutMeContent";
+import ToggleBox from "./components/ToggleBox";
+import ExperienceContent from "./components/ExperienceContent";
+import ProjectsContent from "./components/ProjectsContent";
+import EducationContent from "./components/EducationContent";
+import LightDarkMode from "./components/LightDarkMode";
 
+export const ThemeContext = createContext(null);
 
 export default function App() {
-    
+    const [theme, setTheme] = useState("dark");
+
     return (
-        <div>
+        <ThemeContext.Provider value={{theme, setTheme}}>
+        <div id={theme}>
             <HomePageNavBar />
             <ToggleBox />
             <HomePageContent />
+            <LightDarkMode />
             <HomePageLinks />
             <AboutMeContent />
             <EducationContent />
             <ExperienceContent />
             <ProjectsContent />
         </div>
+        </ThemeContext.Provider>
     );
 }
